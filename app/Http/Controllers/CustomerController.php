e4f84baadac325c17033d6d9f9e91e07e1ef461b
BuddhiEash <buddhieash@gmail.com>
2015-12-23 09:24:36 +0530

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Http\Controllers;
+
+class CustomerController extends Controller {
+
+	public function index()
+	{
+		return view('customer.index');
+	}
+
+}
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.master')
+
+@section('pagetitle')
+Customers
+@stop()
+
+@section('breadcrubms_loc')
+customers
+@stop
+
+@section('content')
+<a href="#">Create new customer</a>
+<br>
+<br>
+<table border="1">
+<!--<input type="text" class="form-control" name="test" placeholder="Text here"/>-->
+<th>Company name</th><th>Address</th><th>Company name</th><th>Business registration number</th><th>Website</th><th>Edit</th>
+<tr>
+	<td>FFF</td>
+</tr>
+</table>
+@stop()
